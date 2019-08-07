9906475d3af61abb1afbba74cd7c4904ccefd66b
arecastudio <ondiisrail@gmail.com>
2019-08-07 15:48:50 +0900

    update laporan-bulanan and style

--- js/pages/laporan-bulanan.js
+++ js/pages/laporan-bulanan.js
@@ -5,9 +5,10 @@ $('.txtanggal').datepicker('option','dateFormat','yy-mm-dd');
 
 $('#btn-show').on('click',function(){
     let jenis=$("input:radio[name=radio-type]:checked").val();
-    console.log('Jenisnya: '+jenis);
+    //console.log('Jenisnya: '+jenis);
 
     let data={};
+    let fdata=new FormData();
 
     if(jenis=='harian'){
 	$('#span-harian').css('display','inline');
@@ -20,9 +21,13 @@ $('#btn-show').on('click',function(){
 	//console.log('Tanggal: '+txtrx);
 
 	data={
+	    'mod':mod,
 	    'act':'harian',
 	    'tanggal':txtrx,
 	}
+	fdata.append('mod',mod);
+	fdata.append('act','harian');
+	fdata.append('tanggal',txtrx);
     }else{
 	//reset hingga di tabel
 	$('#txtrx').val('');
@@ -35,19 +40,79 @@ $('#btn-show').on('click',function(){
 	//console.log('Tgl akhir: '+takhir);
 
 	data={
+	    'mod':mod,
 	    'act':'bulanan',
 	    'tawal':tawal,
 	    'takhir':takhir,
 	}
+	fdata.append('mod',mod);
+	fdata.append('act','bulanan');
+	fdata.append('tawal',tawal);
+	fdata.append('takhir',takhir);
     }
 
-    console.log(data);
+    //console.log(data);
+    //console.log('mod data: '+mod);
+    $.ajax({
+	url:'backend/?',
+	method:'POST',
+	data:fdata,
+	contentType:false,
+	cache:false,
+	processData:false,
+	//contentType: 'multipart/form-data',
+	success:function(resp){
+	    let dx=JSON.parse(resp);
+	    
+	    //console.table(dx['data_tabel']);
+	    
+	    //$('#id-notice-content').show();
+
+
+	    let td='',i=0,gtotal=0,bungkus='&times;';;
+	    for(let x in dx['data_tabel']){
+		i++;
+		td+='';
+		td+='<tr>';
+		td+='<td align="right">'+i+'&nbsp;</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['trx']+'</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['meja']+'</td>';
+		td+='<td>'+dx['data_tabel'][x]['menu']+'</td>';
+		td+='<td align="right">'+dx['data_tabel'][x]['fharga']+'</td>';
+		td+='<td align="right">'+dx['data_tabel'][x]['jumlah']+'</td>';
+		td+='<td align="right">'+dx['data_tabel'][x]['ftotal']+'</td>';
+		if(dx['data_tabel'][x]['bungkus']=='TIDAK'){
+		    bungkus='&times;';
+		}else{
+		    bungkus='&#10004;';
+		}
+		td+='<td align="center">'+bungkus+'</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['jam']+'</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['op']+'</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['kategori']+'</td>';
+		td+='<td align="center">'+dx['data_tabel'][x]['diskon']+'&nbsp;%</td>';
+		td+='</tr>';
+		gtotal+=parseInt(dx['data_tabel'][x]['total']);
+	    }
+	    /*td+='<tr>';
+	    td+='<th colspan="4" align="right">Grand Total (Rp.):</th>';
+	    td+='<th colspan="3" align="right">'+gtotal+'</th>';
+	    td+='<th colspan="5">&nbsp;</th>';
+	    td+='</tr>';*/
+	    $('#id_tbody').html(td);
+	    
+	},
+	error:function(xhr,status,error){
+	    console.log('getting data error');
+	}
+    });
     
 });
 
 $.fn.showResult=function(){
     let jenis=$("input:radio[name=radio-type]:checked").val();
-    console.log('Jenisnya: '+jenis);
+    //console.log('Jenisnya: '+jenis);
+    $('#id_tbody').html('');
 
     if(jenis=='harian'){
 	$('#span-harian').css('display','inline');
