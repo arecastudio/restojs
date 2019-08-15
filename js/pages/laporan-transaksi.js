bd6834d2c1290ac1e5fb2439d671b877fc46a12a
arecastudio <ondiisrail@gmail.com>
2019-08-15 19:47:41 +0900

    update laporan-transaksi table

--- js/pages/laporan-transaksi.js
+++ js/pages/laporan-transaksi.js
@@ -15,7 +15,7 @@ array_bulan={
     12:'Desember',
 };
 
-
+$('#notifikasi').hide();
 //console.log(array_bulan[1]);
 
 let opt='';
@@ -26,10 +26,53 @@ $('#id_optbulan').html(opt);
 
 
 $('#id_btnshow').on('click',function(){
+    $('#notifikasi').show();
     let tahun=$('#id_opttahun').val();
     let bulan=$('#id_optbulan').val();
 
     console.log('Tahun: '+tahun+', Bulan: '+bulan);
+
+    let fdata=new FormData();
+    fdata.append('mod',mod);
+    fdata.append('act','show');
+    fdata.append('tahun',tahun);
+    fdata.append('bulan',bulan);
+
+    console.log(fdata);
+
+    $.ajax({
+	url:'backend/?',
+	method:'POST',
+	data:fdata,
+	contentType:false,
+	cache:false,
+	processData:false,
+	//contentType: 'multipart/form-data',
+	success:function(resp){
+	    //console.log('message: '+resp);
+	    $('#notifikasi').hide();
+	    let dx=JSON.parse(resp);
+	    //console.table(dx);
+
+	    let td='',i=0;
+	    for(let x in dx){
+		i++;
+		td+='<tr>';
+		td+='<td>'+i+'</td>';
+		td+='<td>'+dx[x]['tanggal']+'</td>';
+		td+='<td>'+dx[x]['ftotal_ppn']+'</td>';
+		td+='<td>'+dx[x]['ftotal_non_ppn']+'</td>';
+		td+='<td>'+dx[x]['fppn']+'</td>';
+		td+='<td>'+dx[x]['fjumlah_ppn']+'</td>';
+		td+='</tr>';
+	    }
+	    $('#id_tbody').html(td);
+	},
+	error:function(xhr,status,error){
+	    console.log('getting data error');
+	}
+    });
+    
 });
 
 
