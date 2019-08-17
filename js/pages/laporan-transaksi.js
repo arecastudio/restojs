1bade0b81fd007892740a94b274cc82f8d632699
arecastudio <ondiisrail@gmail.com>
2019-08-17 21:11:53 +0900

    update library list

--- js/pages/laporan-transaksi.js
+++ js/pages/laporan-transaksi.js
@@ -32,7 +32,7 @@ $('#id_btnshow').on('click',function(){
     let bulan=$('#id_optbulan').val();
     let nama_file_xls='laporan_pajak_'+tahun+'_'+bulan;
 
-    console.log('Tahun: '+tahun+', Bulan: '+bulan);
+    //console.log('Tahun: '+tahun+', Bulan: '+bulan);
     $('#id_tampilan').html('');
 
     let fdata=new FormData();
@@ -41,7 +41,7 @@ $('#id_btnshow').on('click',function(){
     fdata.append('tahun',tahun);
     fdata.append('bulan',bulan);
 
-    console.log(fdata);
+    //console.log(fdata);
 
     $.ajax({
 	url:'backend/?',
@@ -92,7 +92,7 @@ $.ajax({
     method:'GET',
     success:function(resp){	
 	let dx=JSON.parse(resp);
-	console.log(dx);
+	//console.log(dx);
 	let tahuns='';
 	for(let x in dx){
 	    tahuns+='<option value="'+dx[x]['tahun']+'">'+dx[x]['tahun']+'</option>';
@@ -113,15 +113,18 @@ $('#id_btnPrint').on('click',function(){
 
 function ExportTable(){
     $("table").tableExport({
-	headings: true,                    // (Boolean), display table headings (th/td elements) in the <thead>
-	footers: true,                     // (Boolean), display table footers (th/td elements) in the <tfoot>
-	//formats: ["xls", "csv", "txt"],    // (String[]), filetypes for the export
+	headers: true,                      // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
+	footers: true,                      // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
+	//formats: ["xlsx", "csv", "txt"],    // (String[]), filetype(s) for the export, (default: ['xlsx', 'csv', 'txt'])
 	formats: ["xls"],
-	fileName: "id",                    // (id, String), filename for the downloaded file
-	bootstrap: false,                   // (Boolean), style buttons using bootstrap
-	position: "well" ,                // (top, bottom), position of the caption element relative to table
-	ignoreRows: null,                  // (Number, Number[]), row indices to exclude from the exported file
-	ignoreCols: null,                 // (Number, Number[]), column indices to exclude from the exported file
-	ignoreCSS: ".tableexport-ignore"   // (selector, selector[]), selector(s) to exclude from the exported file
+	filename: "id",                     // (id, String), filename for the downloaded file, (default: 'id')
+	bootstrap: false,                   // (Boolean), style buttons using bootstrap, (default: true)
+	exportButtons: true,                // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)
+	position: "top",                 // (top, bottom), position of the caption element relative to table, (default: 'bottom')
+	ignoreRows: null,                   // (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
+	ignoreCols: null,                   // (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
+	trimWhitespace: true,               // (Boolean), remove all leading/trailing newlines, spaces, and tabs from cell text in the exported file(s) (default: false)
+	RTL: false,                         // (Boolean), set direction of the worksheet to right-to-left (default: false)
+	sheetname: "id"   
     });
 }
