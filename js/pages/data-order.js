4e19b425a6c2018894aa68c29207285297cf1aa0
arecastudio <ondiisrail@gmail.com>
2019-10-05 07:36:58 +0900

    user auth update #3

--- js/pages/data-order.js
+++ js/pages/data-order.js
@@ -4,7 +4,33 @@ $.ajax({
     url:table_url,
     method:'GET',
     success:function(resp){
-	$('#id_tbody').html(resp);
+	let dx=JSON.parse(resp);
+	//console.table(dx);
+	let div='';
+	for(let x in dx){
+	    if(dx[x]['status']=='KOSONG'){
+		div+='<div class="sub-menu-tables" data-nomor="'+dx[x]['nomor']+'">';
+		div+='<center>';
+		div+='<strong>'+dx[x]['nomor']+'</strong>';
+		div+='<br/>';
+		div+='<img src="images/data-meja.png"/>';
+		div+='<br/>';
+		div+='<small>'+dx[x]['status']+'</small>';
+		div+='</center>';
+		div+='</div>';
+	    }else{
+		div+='<div class="sub-menu-tables" data-nomor="'+dx[x]['nomor']+'" style="border:1px solid red;">';
+		div+='<center>';
+		div+='<strong>'+dx[x]['nomor']+'</strong>';
+		div+='<br/>';
+		div+='<img src="images/meja-terisi.png"/>';
+		div+='<br/>';
+		div+='<small>'+dx[x]['status']+'</small>';
+		div+='</center>';
+		div+='</div>';
+	    }
+	}
+	$('#id_tbody').html(div);
     },
     error:function(xhr,status,error){
 	console.log('getting data error');
