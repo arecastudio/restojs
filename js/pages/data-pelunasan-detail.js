05d580ee6b20abe619b6cd399ff47ad33ae0f05a
arecastudio <ondiisrail@gmail.com>
2019-10-24 10:27:15 +0900

    update pelunasan-detail

--- js/pages/data-pelunasan-detail.js
+++ js/pages/data-pelunasan-detail.js
@@ -2,6 +2,8 @@ const mod="data-pelunasan-detail";
 const txnomor_meja=$('#txnomor-meja').val();
 
 //$('.table-datax').DataTable();
+//$('#id_btnlunaskan').attr('disabled','true');
+//$('#id_btnlunaskan').removeAttr('disabled');
 
 $.fn.resetForms=function(){
     let fdata=new FormData();
@@ -21,6 +23,15 @@ $.fn.resetForms=function(){
 	    //console.log(resp);
 	    let dx=JSON.parse(resp);
 	    console.table(dx);
+
+	    let ed='',j=0;
+	    for(let e in dx['edc']){
+		j++;
+		ed+='<option value="'+dx['edc'][e]['id']+'">';
+		ed+=dx['edc'][e]['nama'];
+		ed+='</option>';
+	    }
+	    $('#id_optbank').append(ed);
 
 	    let dt='',i=0,total=0,grandtotal=0;
 	    for(let d in dx['pesanan']){
@@ -45,8 +56,10 @@ $.fn.resetForms=function(){
 		dt+='</tr>';
 	    }
 
+	    grandtotal=grandtotal+parseInt(dx['tarif']);
+
 	    $('#id_tbody').html(dt);
-	    $('#id_txgtotal').html('Rp. '+grandtotal);
+	    $('#id_txgtotal').html('Rp. '+(grandtotal));
 	    $('#id_txbmeja').html('Rp. '+dx['tarif']);
 	    $('#id_txbilang').html(terbilang(grandtotal));
         },
@@ -59,3 +72,34 @@ $.fn.resetForms=function(){
 $.fn.resetForms();
 console.log('data-pelunasan-detail');
 $('.money').simpleMoneyFormat();
+
+
+$('#id_ctunai').on('click',function(){
+    let _status=$(this).is(':checked');
+    console.log('tunai: '+_status);
+
+    if(_status==true){
+	$('#id_txtunai').removeAttr('disabled');
+    }else{
+	$('#id_txtunai').val('0');
+	$('#id_txtunai').attr('disabled','true');
+    }
+});
+
+$('#id_cnontunai').on('click',function(){
+    let _status=$(this).is(':checked');
+    console.log('tunai: '+_status);
+
+    if(_status==true){
+	$('#id_txnontunai').removeAttr('disabled');
+	$('#id_txnoedc').removeAttr('disabled');
+	$('#id_optbank').removeAttr('disabled');
+    }else{
+	$('#id_txnontunai').val('0');
+	$('#id_txnontunai').attr('disabled','true');
+	$('#id_txnoedc').val('');
+	$('#id_txnoedc').attr('disabled','true');
+	$('#id_optbank').val('');
+	$('#id_optbank').attr('disabled','true');
+    }
+});
