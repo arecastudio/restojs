cde69ef0e3e2d7fe9bd8fc6bcb4057928f6304d6
arecastudio <ondiisrail@gmail.com>
2019-10-27 13:22:06 +0900

    update ubah-order

---
+++ js/pages/ubah-order.js
@@ -0,0 +1,33 @@
+const mod='ubah-order';
+console.log(mod);
+
+$.fn.resetForms=function(){
+    $.ajax({
+        url:'backend/?data='+mod,
+	method:'GET',
+	success:function(resp){
+	    let dx=JSON.parse(resp);
+	    //console.table(dx);
+
+	    let td='',i=0;
+	    for(let x in dx){
+		i++;
+		//td+='';
+		td+='<tr>';
+		//td+='<td class="txcenter">'+i+'</td>';
+		td+='<td class="txcenter">'+dx[x]['meja']+'</td>';
+		td+='<td class="txcenter">'+dx[x]['waktu']+'</td>';
+		td+='<td>'+dx[x]['produks']+'</td>';
+		td+='<td class="txcenter"><button class="btnubah">&larr;</button></td>';
+		td+='</tr>';
+	    }
+
+	    $('#id_tbody').html(td);
+	},
+	error:function(xhr,status,error){
+	    console.log('getting data error');
+	}
+    });
+}
+
+$.fn.resetForms();
