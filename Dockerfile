58cdfa7c7237dd6b79d12305f072dac8bcf717bc
khamali1337 <khamali1337@yandex.ru>
2022-03-28 21:02:26 +0900

    update master

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
-FROM php:apache
-RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+/media/tux/B23A-BB0F/kristo-app/htdocs/restojs/Dockerfile
--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-console.log('Applikasi RestoJS');
-console.log('-----------------');
-
-function formatDesimal(nilai){
-    nilai+='';
-    let val=nilai.split(',').join();
-
-    vals=val.replace(new RegExp(/\B(?=(\d{3})+(?!\d))/g),",");
-    return vals;
-}
+/media/tux/B23A-BB0F/kristo-app/htdocs/restojs/js/main.js
